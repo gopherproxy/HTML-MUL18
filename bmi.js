21681b5745788eaf4755098f361ba16a6980fc8e
gopherproxy <gopherproxy@gmail.com>
2019-03-24 16:43:21 +0100

    Tutorial: BMI calculator with JavaScript and Bootstrap

---
+++ bmi.js
@@ -0,0 +1,19 @@
+/*
+The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m2 (source: Wikipedia)
+
+BMI = weight (kg) / (height * height (m))
+
+Underweight < 18.5
+Normal (healthy weight) 18.5 - 25
+Overweight 25 - 30
+Obesity > 30
+
+*/
+
+// globale variabler (synlige for HELE scriptet!)
+var input, bmi, msg;
+
+function calculateBMI() {
+	'use strict';
+
+}
